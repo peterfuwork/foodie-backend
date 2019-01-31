d0fa21ff529042fd1cc6291b8b5498aedb729afb
Chiaen Fu <peterfuwork@gmail.com>
2019-01-31 14:31:56 -0700

    modified mongodb api var changed

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: process.env.MONGO_URI,
+    MONGODB_URI: process.env.MONGODB_URI,
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
